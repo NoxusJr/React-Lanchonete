d12f34afcb23e346f84296464407a5895b0c9041
NoxusJr <ctt.georgejr@gmail.com>
2024-02-01 23:37:47 -0300

    Favicon e Header

--- meu-projeto/src/components/header.jsx
+++ meu-projeto/src/components/header.jsx
@@ -5,7 +5,7 @@ function Header(){
         <section className={styles.header}>
             <span id={styles.textMain}>
                 <h1 id={styles.h1Main}>Lanchonete</h1>
-                <p id={styles.pMain}>Finge pfv que tem algum texto coerente aqui</p>
+                <p id={styles.pMain}>o melhor lugar você e toda a sua família</p>
             </span>
         </section>
     )
--- meu-projeto/src/components/principal.jsx
+++ meu-projeto/src/components/principal.jsx
@@ -4,8 +4,8 @@ function Principal(){
     return (
         <section className={styles.mainFood}>
             <div id={styles.mainFoodDescricao}>
-                <h1 id={styles.h1Comida}>Comida</h1>
-                <p id={styles.pComida}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus aliquid atque ab excepturi quae illum amet, totam optio molestiae perferendis dolorem fuga a nulla quaerat, facere quibusdam officia mollitia cumque?</p>
+                <h1 id={styles.h1Comida}>Super Combo</h1>
+                <p id={styles.pComida}>Desfrute de uma experiência gastronômica única com nosso Combo Duplo de Hamburguers, incluindo o clássico e o gourmet, acompanhados de batatas fritas. Complete sua refeição com dois refrigerantes refrescantes à sua escolha. Qualidade premium, variedade de sabores e economia em cada mordida. Faça seu pedido agora e aproveite este banquete delicioso!</p>
             </div>
 
             <div id={styles.mainFoodImage}></div>
